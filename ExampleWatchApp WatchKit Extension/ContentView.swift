a0f45da9125ef1106d11191c53957791a9f49ec0
Dario Carlomagno <dario.carlomagno@gmail.com>
2021-05-13 10:49:58 +0200

    Countdown Timer lib

--- ExampleWatchApp WatchKit Extension/ContentView.swift
+++ ExampleWatchApp WatchKit Extension/ContentView.swift
@@ -7,13 +7,15 @@
 
 import SwiftUI
 import Combine
+import CountdownRing
+
 struct ContentView: View {
     
     enum Screen {
         case landing
         case brushing
     }
-
+    
     @State var screen: Screen = .brushing
     
     var body: some View {
@@ -40,28 +42,40 @@ struct Landing: View {
             }
         }
     }
-    
 }
 
 struct Brushing: View {
     
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-        
     @State var isTimerRunning = false
-    
+    @ObservedObject var countdownTimer = CountdownTimer(limitTimeInteraval: 120)
     @State var timeRemaining = 120
-        
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
-        VStack {
-            Text("\(timeRemaining/60):\(timeRemaining%60)")
-                .font(.largeTitle)
+        VStack(alignment: .center, spacing: 8.0) {
+            
+            ZStack {
+                CountdownRingView(
+                    countdownTimer: countdownTimer,
+                    gradientColors: [.green, .blue],
+                    lineWidth: 8
+                )
+                .padding(.bottom, 0.0)
+
+                TimerTextView(countdownTimer: self.countdownTimer)
+            }
+
             if isTimerRunning {
-                Button("Pause") {
+                Button("Reset") {
                     isTimerRunning = false
+                    countdownTimer.reset()
+                    WKInterfaceDevice.current().play(.retry)
                 }
             } else {
                 Button("Start") {
                     isTimerRunning = true
+                    countdownTimer.start()
+                    WKInterfaceDevice.current().play(.start)
                 }
             }
         }
@@ -69,17 +83,23 @@ struct Brushing: View {
             guard isTimerRunning else {
                 return
             }
+            
+            let notifications: [Int: WKHapticType] = [90: .notification,
+                                                      60: .directionDown,
+                                                      30: .directionUp]
+            
             if timeRemaining > 0 {
                 timeRemaining -= 1
+                if let hapticNotification = notifications.first(where: {$0.key == timeRemaining})?.value {
+                    WKInterfaceDevice.current().play(hapticNotification)
+                }
+                
+            } else {
+                WKInterfaceDevice.current().play(.stop)
             }
         }
     }
 }
-
-//class Timer: ObservableObject {
-//
-//    @Published
-//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
