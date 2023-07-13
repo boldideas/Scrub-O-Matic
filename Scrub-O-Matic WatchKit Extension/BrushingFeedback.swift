21e47a57b4fd3a88a474122b0e19bcbc2fdce4c8
Dario Carlomagno <dario.carlomagno@gmail.com>
2023-07-13 17:15:26 +0200

    Updated to always send a `.notification. feedback

--- Scrub-O-Matic WatchKit Extension/BrushingFeedback.swift
+++ Scrub-O-Matic WatchKit Extension/BrushingFeedback.swift
@@ -21,11 +21,19 @@ struct BrushingFeedback {
     }
     
     static func sendTimeElapsingForInterval(_ interval: TimeInterval, elapsingTime: Int) {
+        
         let denominator = 4
         let quarter = interval.fractionedWith(numerator: 1, denominator: denominator).asInt()
         let half = interval.fractionedWith(numerator: 2, denominator: denominator).asInt()
         let threeQuarters = interval.fractionedWith(numerator: 3, denominator: denominator).asInt()
 
+        let intervalsToSendFeedback = [quarter, half, threeQuarters]
+        guard intervalsToSendFeedback.contains(elapsingTime) else {
+            return
+        }
+        WKInterfaceDevice.current().play(.notification)
+        
+        /*
         let notifications: [Int: WKHapticType] = [quarter: .notification,
                                                   half: .directionDown,
                                                   threeQuarters: .directionUp]
@@ -33,5 +41,6 @@ struct BrushingFeedback {
         if let hapticNotification = notifications.first(where: {$0.key == elapsingTime})?.value {
             WKInterfaceDevice.current().play(hapticNotification)
         }
+         */
     }
 }
